a4dc470a6c097358593bbbc076ce68f8e7b817a8
Melissa <melissamichaud0319@gmail.com>
2022-12-09 12:00:19 -0500

    Working automobile forms

--- ghi/app/src/Service/ServiceAppointmentList.js
+++ ghi/app/src/Service/ServiceAppointmentList.js
@@ -3,8 +3,7 @@ import { NavLink } from "react-router-dom"
 
 function ServiceAppointmentList() {
 
-    const [services, setServices] = useState(null);
-
+    const [services, setServices] = useState([]);
     const fetchServices  = async () => {
         const url = 'http://localhost:8080/api/service/';
         const response = await fetch(url);
@@ -16,31 +15,47 @@ function ServiceAppointmentList() {
             console.error(response);
         }
     }
+
+    const [autos, setAutos] = useState([]);
+    const fetchAutomobiles  = async () => {
+        const url = 'http://localhost:8100/api/automobiles/';
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            setAutos(data.autos);
+        } else {
+            console.error(response);
+        }
+    }
+
+    useEffect(() => {
+        fetchServices();
+        fetchAutomobiles();
+    }, []);
+
     const cancelService = async (id) => {
         const cancelUrl = `http://localhost:8080/api/service/${id}/`
-        const response = await fetch(cancelUrl, {method: 'DELETE'});
-        fetchServices()
+        await fetch(cancelUrl, {method: 'DELETE'});
+        setServices(services.filter(function(service){return service.id !== id}))
+        // fetchServices()
     }
 
     const finishService = async (id) => {
         const finishUrl = `http://localhost:8080/api/service/${id}/`
-        const status = {"status": "COMPLETE"}
         const fetchConfig = {
-            method: "put",
-            body: JSON.stringify(status),
+            method: "PUT",
+            body: JSON.stringify(id),
             headers: {
                 'Content-Type': 'application/json',
             },
         };
         const response = await fetch(finishUrl, fetchConfig);
-        // fetchServices()
     }
 
-    useEffect(() => {
-        fetchServices()
-    }, []);
 
 
+    let vip = "d-none"
     return (
     <div className="container">
         <div className="col-md-12 text-center">
@@ -54,31 +69,35 @@ function ServiceAppointmentList() {
         <table className="table table-striped">
             <thead>
                 <tr>
+                    <th>VIP</th>
                     <th>VIN</th>
                     <th>Owner</th>
                     <th>Date and Time of Appt</th>
                     <th>Technician</th>
                     <th>Reason</th>
-                    <th>VIP</th>
                 </tr>
             </thead>
             <tbody>
-                {services?.filter((service) => service.status !== "COMPLETE") ?.map(service => {
-                        let date = Date.parse(service.starts)
+                {services?.map(service => {
+                    let date = Date.parse(service.starts)
                         const newDate = Date(date)
-
-                        let is_vip = ''
-                        if (service.vip == true) {
-                            is_vip = <i class="bi bi-car-front-fill"></i>
-                        }
                     return (
                         <tr key={service.id}>
+                            {autos?.map(auto => {
+                                if (service.vin == auto.vin){
+                                    return(
+                                        <td key={service.id}><img src="https://image.emojipng.com/675/36675.jpg" width="30" height="30"/></td>
+                                    )
+                                } else {
+                                    return(<td key={service.id}><img src="https://uxwing.com/wp-content/themes/uxwing/download/checkmark-cross/red-x-icon.png" width="30" height="30"/></td>
+                                    )
+                                }
+                            })}
                             <td>{ service.vin }</td>
                             <td>{ service.vehicle_owner }</td>
                             <td>{ newDate.toLocaleString()}</td>
                             <td>{ service.technician.name }</td>
                             <td>{ service.reason }</td>
-                            <td><i className = {is_vip}></i></td>
                             <td><button onClick={ () => cancelService(service.id)}
                                 className="btn btn-danger">Cancel</button></td>
                             <td><button onClick={ () => finishService(service.id)}
