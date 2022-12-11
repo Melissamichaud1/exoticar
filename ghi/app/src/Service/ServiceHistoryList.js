86c7d02a5ecbf8cd29d58bd46b7efbba5f1bb50e
Melissa <melissamichaud0319@gmail.com>
2022-12-11 16:36:42 -0500

    Finished services yayayayay

--- ghi/app/src/Service/ServiceHistoryList.js
+++ ghi/app/src/Service/ServiceHistoryList.js
@@ -25,11 +25,12 @@ class ServiceHistoryList extends React.Component {
         this.setState({ search: value })
     }
 
+
     render(){
         return (
                 <div>
                 &nbsp;&nbsp;&nbsp;
-                    <div class="input-group">
+                    <div className="input-group">
                     <input onChange={this.handleSearchChange} type="search" className="form-control" placeholder="Search VIN"/>
                 </div>
                 <div className="col-md-12 text-center">
@@ -42,10 +43,11 @@ class ServiceHistoryList extends React.Component {
                                 <th>Date and Time of Appt</th>
                                 <th>Technician</th>
                                 <th>Reason</th>
+                                <th>Finished</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {this.state.services.filter((service) => service.vin.includes(this.state.search) && service.finished == true).map((service) => {
+                            {this.state.services.filter((service) => service.vin.includes(this.state.search)).map((service) => {
 
                                 let date = Date.parse(service.starts)
                                 const newDate = Date(date)
@@ -57,6 +59,8 @@ class ServiceHistoryList extends React.Component {
                                     <td>{ newDate.toLocaleString()}</td>
                                     <td>{ service.technician.name }</td>
                                     <td>{ service.reason }</td>
+                                    {service.finished && <td>Yes</td>}
+                                    {!service.finished && <td>No</td>}
                                 </tr>
                                 )
                             })}
